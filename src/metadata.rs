aaec073c34cad50015f40d27e8cf081933040ce2
Exiua <master6969697@gmail.com>
2025-10-26 19:19:34 -0500

    replace version string with version struct

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 
+use crate::semver::Version;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FsvMetadata {
-    pub format_version: String,
+    pub format_version: Version,
     #[serde(default)]
     pub tags: Vec<String>,
     pub title: String,
@@ -38,24 +40,42 @@ pub struct CreatorInfo {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct VideoFormat {
     pub name: String,
+    #[serde(default)]
     pub description: String,
+    #[serde(default)]
+    pub duration_ms: u64,
+    #[serde(default)]
+    pub start_offset_ms: u64,
+    #[serde(default)]
     pub checksum: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ScriptVariant {
     pub name: String,
+    #[serde(default)]
     pub description: String,
+    #[serde(default)]
     pub additional_axes: Vec<String>,
+    #[serde(default)]
+    pub duration_ms: u64,
+    #[serde(default)]
+    pub start_offset_ms: u64,
+    #[serde(default)]
     pub checksum: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SubtitleTrack {
     pub name: String,
+    #[serde(default)]
     pub language: String,
+    #[serde(default)]
     pub description: String,
+    #[serde(default)]
     pub duration_ms: u64,
+    #[serde(default)]
     pub start_offset_ms: u64,
+    #[serde(default)]
     pub checksum: String,
 }
